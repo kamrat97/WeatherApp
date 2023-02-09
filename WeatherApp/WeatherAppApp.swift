ef62ef06f15e636dfa460b57b3a53c7748a58d66
kamrat97 <44671712+kamrat97@users.noreply.github.com>
2023-02-09 15:28:46 +0700

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Влад on 09.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
